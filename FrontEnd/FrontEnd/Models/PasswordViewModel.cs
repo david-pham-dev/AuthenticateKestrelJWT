1dc6eb599b3f94e5b7c64f13c21f9b18aa6037db
Minh Duc <minhd3193@gmail.com>
2025-03-31 14:33:49 +1100

    Add Show/Display password

---
+++ FrontEnd/FrontEnd/Models/PasswordViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+
+namespace FrontEnd.Models
+{   
+    public partial class PasswordViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string password;
+
+        [ObservableProperty]
+        private string passwordToggleIcon ="\uf06e";
+
+        [ObservableProperty]
+        private bool isPasswordHidden = true;
+        public PasswordViewModel()
+        {
+            IsPasswordHidden = true;
+        }
+        [RelayCommand]
+        void TogglePassword()
+        {
+            IsPasswordHidden = !IsPasswordHidden;
+            PasswordToggleIcon = IsPasswordHidden ? "\uf06e" : "\uf070";
+        }
+    }
+}
